b4c333cc0839e764820692f680f03eeb6c83cefd
pocokb <pocokb@gmail.com>
2017-04-19 13:25:06 +0200

    a

--- controller/klub.php
+++ controller/klub.php
@@ -1,7 +1,8 @@
 <?php
 require_once 'model/david.php';
 $login = 'pocokb@gmail.com';
-$users = getUsers();
+$klubNev = 
+$klub = getKlub($nev)
 $self = getUser($login);
 $clubsOfUser = getClubsOfUser($login);
 include_once 'view/index.html';
--- model/david.php
+++ model/david.php
@@ -5,8 +5,35 @@ function getUsers(){
 	$st = oci_parse($conn,$query);
 	oci_execute($st) or die();
 	oci_fetch_all($st, $res, null, null, OCI_FETCHSTATEMENT_BY_ROW);
+    
+    oci_free_statement($st);
+    oci_close($conn);
 	return $res;		
 }
-
-	
+function getUser($email){
+	require 'sqlconnect.php';
+	$query = 'SELECT * FROM Felhasznalo WHERE email = :email';				
+	$st = oci_parse($conn,$query);
+    oci_bind_by_name($st, ':email', $email);
+	oci_execute($st) or die();
+	$user = oci_fetch_array($st, OCI_ASSOC);
+    
+    oci_free_statement($st);
+    oci_close($conn);
+	return $user;		
+}
+function getClubsOfUser($email){
+    require 'sqlconnect.php';
+	$query = 'SELECT klub_nev FROM klub_tagja WHERE tag_email = :email';				
+    $st = oci_parse($conn,$query);
+	oci_bind_by_name($st, ':email', $email);
+    
+    oci_execute($st) or die();
+	oci_fetch_all($st, $res, null, null, OCI_FETCHSTATEMENT_BY_ROW);    
+    
+    oci_free_statement($st);
+    oci_close($conn);
+        
+	return $res;   
+}
 ?>
